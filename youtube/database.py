92f3f614502f508c32bb07e6df2fd34d9aa7cc04
chimezdev <ochuba6@gmail.com>
2024-01-12 23:32:40 +0100

    many-to-many relationship

--- youtube/database.py
+++ youtube/database.py
@@ -95,80 +95,138 @@
 
 
 
-# Database relationship
-import xml.etree.ElementTree as ET
+# # Database relationship
+# import xml.etree.ElementTree as ET
+# import sqlite3
+
+# conn = sqlite3.connect('trackdb.sqlite')
+# cur = conn.cursor()
+
+# #Make some fresh tables using executescript()
+# cur.executescript('''
+#                   DROP TABLE IF EXISTS Artist;
+#                   DROP TABLE IF EXISTS Album;
+#                   DROP TABLE IF EXISTS Track;
+                  
+#                   CREATE TABLE Artist (
+#                       id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+#                       name TEXT UNIQUE
+#                   );
+                  
+#                   CREATE TABLE Album (
+#                       id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+#                       artist_id INTEGER,
+#                       title TEXT UNIQUE
+                      
+#                   );
+                  
+#                   CREATE TABLE Track (
+#                       id INTEGER NOT NULL PRIMARY KEY
+#                       AUTOINCREMENT UNIQUE,
+#                       title TEXT UNIQUE,
+#                       album_id INTEGER,
+#                       len INTEGER, rating INTEGER, count INTEGER
+#                   );
+#                   ''')
+
+# fname = input('Enter file name: ')
+# if (len(fname) < 1): fname = 'Library.xml'
+
+# #<key>Track ID</key><integer>369</integer>
+# #<key>Name</key><string>Another One Bites The Dust</string>
+# #<key>Artist</key><string>Queen</string>
+# def lookup(d, key):
+#     found = False
+#     for child in d:
+#         if found: return child.text
+#         if child.tag == 'key' and child.text == key:
+#             found = True
+#     return None
+
+# stuff = ET.parse(fname)
+# all = stuff.findall('dict/dict/dict')
+# print('Dict count:', len(all))
+# for entry in all:
+#     if (lookup(entry, 'Track ID') is None ): continue
+    
+#     name = lookup(entry, 'Name')
+#     artist = lookup(entry, 'Artist')
+#     album = lookup(entry, 'Album')
+#     count = lookup(entry, 'Play Count')
+#     rating = lookup(entry, 'Rating')
+#     length = lookup(entry, 'Total Time')
+    
+#     if name is None or artist is None or album is None:
+#         continue
+#     print(name, artist, album, count, rating, length)
+    
+#     cur.execute('''INSERT OR IGNORE INTO Artist (name) VALUES (?)''', (artist,))
+#     cur.execute('SELECT id FROM Artist WHERE name = ?', (artist, ))
+#     artist_id = cur.fetchone()[0]
+    
+#     cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) VALUES (?, ?)''', (album, artist_id))
+#     cur.execute('SELECT id FROM Album WHERE title = ?', (album, ))
+#     album_id = cur.fetchone()[0]
+    
+#     cur.execute('''INSERT OR REPLACE INTO Track (title, album_id, len, rating, count) VALUES (?, ?, ?, ?, ?)''', (name, album_id, length, rating, count))
+    
+#     conn.commit()
+    
+    
+
+# Many to Many Relationship
+import json
 import sqlite3
 
-conn = sqlite3.connect('trackdb.sqlite')
+conn = sqlite3.connect('rosterdb.sqlite')
 cur = conn.cursor()
 
-#Make some fresh tables using executescript()
+# Do some setup
 cur.executescript('''
-                  DROP TABLE IF EXISTS Artist;
-                  DROP TABLE IF EXISTS Album;
-                  DROP TABLE IF EXISTS Track;
+                  DROP TABLE IF EXISTS User;
+                  DROP TABLE IF EXISTS Member;
+                  DROP TABLE IF EXISTS Course;
                   
-                  CREATE TABLE Artist (
+                  CREATE TABLE User (
                       id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
                       name TEXT UNIQUE
                   );
                   
-                  CREATE TABLE Album (
+                  CREATE TABLE Course (
                       id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-                      artist_id INTEGER,
                       title TEXT UNIQUE
-                      
                   );
                   
-                  CREATE TABLE Track (
-                      id INTEGER NOT NULL PRIMARY KEY
-                      AUTOINCREMENT UNIQUE,
-                      title TEXT UNIQUE,
-                      album_id INTEGER,
-                      len INTEGER, rating INTEGER, count INTEGER
-                  );
+                  CREATE TABLE Member (
+                      user_id INTEGER,
+                      course_id INTEGER,
+                      role INTEGER,
+                      PRIMARY KEY (user_id, course_id)
+                  )
                   ''')
 
 fname = input('Enter file name: ')
-if (len(fname) < 1): fname = 'Library.xml'
+if len(fname) < 1:
+    fname = 'roster_data_sample.json'
 
-#<key>Track ID</key><integer>369</integer>
-#<key>Name</key><string>Another One Bites The Dust</string>
-#<key>Artist</key><string>Queen</string>
-def lookup(d, key):
-    found = False
-    for child in d:
-        if found: return child.text
-        if child.tag == 'key' and child.text == key:
-            found = True
-    return None
+str_data = open(fname).read()
+json_data = json.loads(str_data)
 
-stuff = ET.parse(fname)
-all = stuff.findall('dict/dict/dict')
-print('Dict count:', len(all))
-for entry in all:
-    if (lookup(entry, 'Track ID') is None ): continue
+for entry in json_data:
+    name= entry[0]
+    title = entry[1]
     
-    name = lookup(entry, 'Name')
-    artist = lookup(entry, 'Artist')
-    album = lookup(entry, 'Album')
-    count = lookup(entry, 'Play Count')
-    rating = lookup(entry, 'Rating')
-    length = lookup(entry, 'Total Time')
+    print((name, title))
+
+    cur.execute('''INSERT OR IGNORE INTO User (name) VALUES (?)''', (name,))
+    cur.execute('SELECT id FROM User WHERE name = ?', (name, ))
+    user_id = cur.fetchone()[0]
     
-    if name is None or artist is None or album is None:
-        continue
-    print(name, artist, album, count, rating, length)
+    cur.execute('''INSERT OR IGNORE INTO Course (title) VALUES (?)''', (title, ))
+    cur.execute('SELECT id FROM Course WHERE title = ?', (title, ))
+    course_id = cur.fetchone()[0]
     
-    cur.execute('''INSERT OR IGNORE INTO Artist (name) VALUES (?)''', (artist,))
-    cur.execute('SELECT id FROM Artist WHERE name = ?', (artist, ))
-    artist_id = cur.fetchone()[0]
-    
-    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) VALUES (?, ?)''', (album, artist_id))
-    cur.execute('SELECT id FROM Album WHERE title = ?', (album, ))
-    album_id = cur.fetchone()[0]
-    
-    cur.execute('''INSERT OR REPLACE INTO Track (title, album_id, len, rating, count) VALUES (?, ?, ?, ?, ?)''', (name, album_id, length, rating, count))
+    cur.execute('''INSERT OR REPLACE INTO Member (user_id, course_id) VALUES (?, ?)''', (user_id, course_id))
     
     conn.commit()
     
