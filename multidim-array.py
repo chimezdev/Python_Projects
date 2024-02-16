f3bfedf3de158305ca47b99c4f20c8092abc919a
chimezdev <ochuba6@gmail.com>
2024-02-16 18:13:24 +0100

    all functions defined

--- multidim-array.py
+++ multidim-array.py
@@ -1,34 +1,46 @@
-# A device records the air temperature on an hourly basis and does it throughout the month. This gives you a total of 24 × 31 = 744 values.
-# the thermometer measures to 0.1C accuracy so we adopt a float.
-# Let each row of the matrix represent the hourly reading therefore will have 24 elements and we need 31 rows
-# To determine the monthly average noon temperature. Add up all 31 readings recorded at noon and divide the sum by 31. 
-# You can assume that the midnight temperature is stored first. Here's the relevant code:
+# # A device records the air temperature on an hourly basis and does it throughout the month. This gives you a total of 24 × 31 = 744 values.
+# # the thermometer measures to 0.1C accuracy so we adopt a float.
+# # Let each row of the matrix represent the hourly reading therefore will have 24 elements and we need 31 rows
+# # To determine the monthly average noon temperature. Add up all 31 readings recorded at noon and divide the sum by 31. 
+# # You can assume that the midnight temperature is stored first. Here's the relevant code:
 
-temps = [[0.0 for h in range(24)] for d in range(31)]
-#print(temps)
+# temps = [[0.0 for h in range(24)] for d in range(31)]
+# #print(temps)
 
-# monthly average temperature at noon
-# count the days when the temp at noon was at least 20C
-total = 0.0
-count = 0
-for day in temps:
-    total += day[11]
-    if day[11] >= 20.0:
-        count += 1
+# # monthly average temperature at noon
+# # count the days when the temp at noon was at least 20C
+# total = 0.0
+# count = 0
+# for day in temps:
+#     total += day[11]
+#     if day[11] >= 20.0:
+#         count += 1
 
-print("%d days were hot" % count)
-average = total / 31
-print("Average temp at noon:", average)
+# print("%d days were hot" % count)
+# average = total / 31
+# print("Average temp at noon:", average)
 
-# highest temperature during the whole months
-highest_temp = -100
-for day in temps:
-    for i in day:
-        if i <= highest_temp:
-            continue
-        else:
-            highest_temp = i
-print(" %d is the highest monthly temp recorded" % highest_temp)
+# # highest temperature during the whole months
+# highest_temp = -100
+# for day in temps:
+#     for i in day:
+#         if i >= highest_temp:
+#             continue
+#         else:
+#             highest_temp = i
+# print(" %d is the highest monthly temp recorded" % highest_temp)
 
-#Three-dimensional array
-# rooms = [[[False for r in range(20)] for f in range(15)] for t in range(3)]
+# #Three-dimensional array
+# # rooms = [[[False for r in range(20)] for f in range(15)] for t in range(3)]
+
+try:
+    value = input("Enter a value: ")
+    print(value/value)
+except ValueError:
+    print("Bad input...")
+except ZeroDivisionError:
+    print("Very bad input...")
+except TypeError:
+    print("Very very bad input...")
+except:
+    print("Booo!")
---
+++ tic-tac-rev.py
@@ -0,0 +1,114 @@
+# Tic-Tac-Toe game written in python
+
+from random import randrange
+
+board = [ [i + 3 * (j - 1) for i in range(1, 4)] for j in range(1, 4) ] # make an empty board
+
+def display_board(board):
+    print("+-------" * 3, "+", sep="")
+    for row in range(3):
+        print("|       " * 3, "|", sep="")
+        for col in range(3):
+            print("|   " + str(board[row][col]) + "   ", end="")
+        print("|")
+        print("|       " * 3,"|",sep="")
+        print("+-------" * 3,"+",sep="")
+
+print("WELCOME!")
+display_board(board)
+
+# comp = randrange(1, 9)
+def enter_move(board):
+    while True:
+        try:
+            user = int(input("Your turn, 'Make a move by entering the box number': "))
+            found = False
+            for i, row in enumerate(board):
+                for j, col in enumerate(row):
+                    if user == col:
+                        board[i][j] = "O"
+                        found = True
+                        break
+            if not found:
+                print("Invalid move! Please choose a box with a number: ")
+            else: break
+        except ValueError as e:
+            print("Invalid move!, 'Enter a valid box number': ")
+
+def draw_move(board):
+    while True:
+        found = False
+        comp = randrange(1, 10)
+        for i, row in enumerate(board):
+            for j, col in enumerate(row):
+                if comp == col:
+                    board[i][j] = 'X'
+                    found = True
+                    break
+      # print('me', comp)
+        if not found:
+            continue
+        else: break
+draw_move(board)
+display_board(board)
+
+def make_list_of_free_fields(board):
+    # The function browses the board and builds a list of all the free squares; 
+    # the list consists of tuples, while each tuple is a pair of row and column numbers.
+    empty_sq =list()
+    for i, row in enumerate(board):
+        for j, col in enumerate(row):
+            if board[i][j] in range(10):
+                empty_sq.append((i, j))
+
+def victory_for(board, sign):
+    # The function analyzes the board's status in order to check if 
+    # the player using 'O's or 'X's has won the game
+
+    if sign == 'X':
+        who = 'me'
+    elif sign == 'O':
+        who = 'you'
+    else: who = None
+
+    # Check rows
+    for row in board:
+        if all(element == sign for element in row):
+            return who
+
+    # Check columns
+    for col in range(3):
+        if all(board[i][col] == sign for i in range(3)):
+            return who
+
+    # Check diagonals
+    if all(board[i][i] == sign for i in range(3)):
+        return who
+    if all(board[i][2-i] == sign for i in range(3)):
+        return who
+
+    return None
+            
+
+print("I play first")
+board[1][1] = "X"
+display_board(board)
+
+enter_move(board)
+display_board(board)
+
+make_list_of_free_fields(board)
+
+
+
+    # for i in range(3):
+    #     for j in range(3):
+    #         if board[i][j] == board[0][0] == board[0][1] == board[0][2] == "X" or \
+    #         board[i][j] == board[1][0] == board[1][1] == board[1][2] == "X" or \
+    #         board[i][j] == board[2][0] == board[2][1] == board[2][2] == "X" or \
+    #         board[i][j] == board[0][0] == board[1][0] == board[2][0] == "X" or \
+    #         board[i][j] == board[0][1] == board[1][1] == board[2][1] == "X" or \
+    #         board[i][j] == board[0][2] == board[1][2] == board[2][2] == "X" or \
+    #         board[i][j] == board[0][0] == board[1][1] == board[2][2] == "X" or \
+    #         board[i][j] == board[0][2] == board[1][1] == board[2][0] == "X":
+    #             print("I won!")
