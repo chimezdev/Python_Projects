758ae9bf08d2080851df41bed38eb99877337de1
chimezdev <ochuba6@gmail.com>
2024-02-28 02:07:02 +0100

    improved caesar cipher

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,16 +1,30 @@
 #Encryption method used by the acient Roman soldiers
-# modified to assume that the romans used numbers,  and spaceing
+# modified to assume that the romans used numbers, lower-case letters and spaceing
 
 text = input("Enter your message: ")
+try:
+    shift = int(input("Enter encryption value(1-25): "))
+    while shift not in range(1, 26):
+        shift = int(input("Enter number between 1-25: "))
+except ValueError as e:
+    print("Encryption value must be an integer number btw 1-25!", e)
+
 cipher = ''
 for char in text:
     if not char.isalpha():
         cipher += char
         continue
-    char = char.upper()
-    code = ord(char) + 1
-    if code > ord('Z'):
-        code = ord('A')
-    cipher += chr(code)
+    #char = char.upper()
+    if char.islower():
+        code = ord(char) + shift
+        if code > ord('z'):
+            code = (code - ord('z') - 1) + ord('a')
+        cipher += chr(code)
 
+    if char.isupper():
+        code = ord(char) + shift
+        if code > ord('Z'):
+            code = (code - ord('Z') - 1) + ord('A')
+        cipher += chr(code)
+        
 print(cipher)
