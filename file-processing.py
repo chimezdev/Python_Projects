5dcd344dff368518eb1b6eed3bd40f08ca1b2ad7
chimezdev <ochuba6@gmail.com>
2024-03-03 18:42:48 +0100

    copying bitmap file

--- file-processing.py
+++ file-processing.py
@@ -48,14 +48,64 @@ import sys
 #     print(hex(b))
     
 
-# Writing a byte array to a binary file
-data = bytearray(10)
-for dat in range(len(data)):
-    data[dat] = 10 + dat
+# # Writing a byte array to a binary file
+# data = bytearray(10)
+# for dat in range(len(data)):
+#     data[dat] = 10 + dat
 
+# try:
+#     bytefile = open('file.bin', 'wb')
+#     bytefile.write(data)
+#     bytefile.close()
+# except IOError as e:
+#     print("I/O error occured:", strerror(e.errno))
+
+
+# # Reading binary file using 'readinto()' method
+# # data = bytearray(10)
+# try:
+#     binary_file = open('file.bin', 'rb')
+#    # binary_file.readinto(data)      
+#     data = bytearray(binary_file.read(5)) #if used, 'data = bytearray(10)' is needed
+#     binary_file.close()
+
+#     for b in data:
+#         print(hex(b), end=' ')
+# except IOError as e:
+#     print("I/O error occured:", strerror(e.errno))
+
+
+
+# COPYING FILE
+from os import strerror
+
+srcname = input("Enter the source file: ")
 try:
-    bytefile = open('file.bin', 'wb')
-    bytefile.write(data)
-    bytefile.close()
+    src = open(srcname, 'rb')
 except IOError as e:
-    print("I/O error occured:", strerror(e.errno))
+    print("Cannot open the source file: ", strerror(e.errno))
+    exit(e.errno)
+
+dstname = input("Enter the destination file name: ")
+try:
+    dst = open(dstname, 'wb')
+except Exception as e:
+    print("Cannot create the destination file: ", strerror(e.errno))
+    src.close()
+    exit(e.errno)
+
+buffer = bytearray(65536)
+total = 0
+try:
+    readin = src.readinto(buffer)
+    while readin > 0:
+        written = dst.write(buffer[:readin])
+        total += written
+        readin = src.readinto(buffer)
+except IOError as e:
+    print("Cannot create the destination file: ", strerror(e.errno))
+    exit(e.errno)
+
+print(total,'byte(s) succesfully written')
+src.close()
+dst.close()
