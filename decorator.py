2f05285deca24e84301b592e753300ade8ba0255
chimezdev <ochuba6@gmail.com>
2024-03-17 12:52:42 +0100

    application of decorator

--- decorator.py
+++ decorator.py
@@ -23,19 +23,43 @@
 # display_info('John', 30)
 
 
-# PRACTICAL APPLICATION OF Decorator is in logging
-def my_logger(orig_func):
-    import logging
-    logging.basicConfig(filename='{}'.format(orig_func.__name__), level=logging.INFO)
+# PRACTICAL APPLICATION OF Decorator
+# # in logging
+# def my_logger(orig_func):
+#     import logging
+#     logging.basicConfig(filename='{}'.format(orig_func.__name__), level=logging.INFO)
+
+#     def wrapper(*args, **kwargs):
+#         logging.info('Ran with args: {}, and kwargs: {}'.format(args, kwargs))
+#         return(orig_func(*args, **kwargs))
+    
+#     return wrapper
+
+# @my_logger
+# def display_info(name, age):
+#     print('display_info ran with argumens ({}, {})'.format(name, age))
+
+# display_info('John', 25)
+
+
+
+# In how long a function runs
+import time
+def my_timer(orig_func):
+    import time
 
     def wrapper(*args, **kwargs):
-        logging.info('Ran with args: {}, and kwargs: {}'.format(args, kwargs))
-        return(orig_func(*args, **kwargs))
+        t1 = time.time()
+        result = orig_func(*args, **kwargs)
+        t2 = time.time() - t1
+        print('{} ran in: {} sec'.format(orig_func.__name__, t2))
+        return result
     
     return wrapper
 
-@my_logger
+@my_timer
 def display_info(name, age):
-    print('display_info ran with argumens ({}, {})'.format(name, age))
+    time.sleep(1)
+    print('display_info ran with arguments ({}, {})'.format(name, age))
 
-display_info('John', 25)
+display_info("Stone", 30)
